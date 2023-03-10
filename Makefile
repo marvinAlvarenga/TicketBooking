ca37f86765f86434866d5369ceca49f5722e1405
Marvin Alvarenga <marvinalexz@gmail.com>
2023-03-10 11:18:18 -0600

    Add formatting with black

--- Makefile
+++ Makefile
@@ -4,3 +4,5 @@ test:
 	docker compose run --rm api ./scripts/tests.sh
 lint:
 	docker compose run --rm api ./scripts/lint.sh
+format:
+	docker compose run --rm api ./scripts/format.sh
--- roombooking/main.py
+++ roombooking/main.py
@@ -1,11 +1,11 @@
 from fastapi import FastAPI
 
 app = FastAPI(
-    title='RoomBookingApi',
-    version='0.1.0',
+    title="RoomBookingApi",
+    version="0.1.0",
 )
 
 
-@app.get('/')
+@app.get("/")
 def health_check() -> str:
-    return 'Ok!'
+    return "Ok!"
