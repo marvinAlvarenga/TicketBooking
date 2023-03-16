365a40592ea35dce64bb7ec18396ee58ab736dbe
Marvin Alvarenga <marvinalexz@gmail.com>
2023-03-16 17:39:22 -0600

    Add extra info to events table

--- ticketbooking/api/events/models.py
+++ ticketbooking/api/events/models.py
@@ -1,4 +1,4 @@
-from sqlalchemy import CheckConstraint, Column, Integer, String, Text
+from sqlalchemy import CheckConstraint, Column, DateTime, Integer, String, Text
 
 from ticketbooking.common.models import AuditModel, IdentityModel
 from ticketbooking.config.db import Base
@@ -6,6 +6,10 @@ from ticketbooking.config.db import Base
 
 class EventModel(Base, IdentityModel, AuditModel):
     __tablename__ = "events"
+
     title = Column(String(255), nullable=False)
     description = Column(Text, nullable=True)
+    address = Column(String(255), nullable=True)
     total_tickets = Column(Integer, CheckConstraint("total_tickets >= 0"))
+    starts_at = Column(DateTime(timezone=True), nullable=True)
+    ends_at = Column(DateTime(timezone=True), nullable=True)
