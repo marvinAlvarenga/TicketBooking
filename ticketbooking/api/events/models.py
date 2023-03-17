bd980b2a8442f267e7ceee347d32fb4064a9b180
Marvin Alvarenga <marvinalexz@gmail.com>
2023-03-16 18:08:53 -0600

    Add EventType relationship

--- ticketbooking/api/events/models.py
+++ ticketbooking/api/events/models.py
@@ -1,4 +1,13 @@
-from sqlalchemy import CheckConstraint, Column, DateTime, Integer, String, Text
+from sqlalchemy import (
+    CheckConstraint,
+    Column,
+    DateTime,
+    ForeignKey,
+    Integer,
+    String,
+    Text,
+)
+from sqlalchemy.orm import relationship
 
 from ticketbooking.common.models import AuditModel, IdentityModel
 from ticketbooking.config.db import Base
@@ -13,3 +22,15 @@ class EventModel(Base, IdentityModel, AuditModel):
     total_tickets = Column(Integer, CheckConstraint("total_tickets >= 0"))
     starts_at = Column(DateTime(timezone=True), nullable=True)
     ends_at = Column(DateTime(timezone=True), nullable=True)
+
+    event_type_id = Column(
+        Integer,
+        ForeignKey("events_type.id"),
+        nullable=True,
+    )
+    event_type = relationship("EventTypeModel")
+
+
+class EventTypeModel(Base, IdentityModel, AuditModel):
+    __tablename__ = "events_type"
+    name = Column(String(127), nullable=False)
